d80269dc23f3ab01548705442d2f7d3e66d2d71a
jncogs <cogswell.jesse@gmail.com>
2025-06-22 12:25:31 -0500

    Added player and circleshape classes

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -15,8 +19,11 @@ def main():
                 return
             
         screen.fill("black")
-        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
+        player.draw(screen)
+
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
